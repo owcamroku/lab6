a7bf0cc27a00c50a05147512e4df1800c8c3ddbe
RozoweKucyki <krzysztof.kusiak97@wp.pl>
2019-01-16 23:06:46 +0100

    Dodano pliki: wersy_1-2.hpp oraz wersy_1-2.cpp

---
+++ wersy_1-2.cpp
@@ -0,0 +1,7 @@
+//wersy_1-2.cpp
+#include "wersy_1-2.hpp"
+
+void wyswietl_wersy_1-2(){
+cout<<endl<<"Nie wchodź łagodnie do tej dobrej nocy"<<endl;
+cout<<"Starość u kresu dnia niech płonie, krwawi"<<endl;
+}
---
+++ wersy_1-2.hpp
@@ -0,0 +1,8 @@
+// wersy_1-2
+
+#ifndef wersy_1-2_hpp
+#define wersy_1-2_hpp
+
+void wyswietl_wersy_1-2();
+
+#endif
