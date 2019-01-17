5e609415ce13909ddbae962cd2b1b8388e28549a
RozoweKucyki <krzysztof.kusiak97@wp.pl>
2019-01-17 01:35:17 +0100

    edycja: wersy.cpp

--- wersy.cpp
+++ wersy.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void wyswietl_wersy(){
 
 cout<<endl<<"Nie wchodź łagodnie do tej dobrej nocy"<<endl;
-cout<<"Starość u kresu dnia niech płonie, krwawi"<<endl;
+cout<<"Starość u kresu dnia niech krwawi, płonie"<<endl;
 cout<<"Buntuj się, buntuj, gdy światło się mroczy"<<endl;
 
 }
