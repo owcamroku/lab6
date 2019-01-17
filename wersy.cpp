038fe7e32909f8e9812d194793f6c6de8da1292a
owcamroku <46757508+owcamroku@users.noreply.github.com>
2019-01-17 03:49:31 +0100

    Create wersy.cpp

---
+++ wersy.cpp
@@ -0,0 +1,18 @@
+//wersy.cpp
+
+#include "wersy.hpp"
+#include <iostream>
+
+using namespace std;
+
+void wyswietl_wersy(){
+
+cout<<endl<<"Nie wchodź łagodnie do tej dobrej nocy"<<endl;
+<<<<<<< HEAD
+cout<<"Starość u kresu dnia niech płonie, krwawi"<<endl;
+=======
+cout<<"Starość u kresu dnia niech płonie, krwawi"<<endl;
+>>>>>>> 5e609415ce13909ddbae962cd2b1b8388e28549a
+cout<<"Buntuj się, buntuj, gdy światło się mroczy"<<endl;
+
+}
